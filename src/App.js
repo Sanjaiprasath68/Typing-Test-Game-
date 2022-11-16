16c27a57921d72ac5478d927183f9c2afcfb65a4
bhabishya <bhabishyaghimire88@gmail.com>
2022-11-16 21:09:08 +0545

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SpeedTester from './component/speedTester/SpeedTester';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <SpeedTester/>
+
+    </div>
+  );
+}
+
+export default App;
